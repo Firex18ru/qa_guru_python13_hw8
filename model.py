3f12ccb90d686ca2c8911a6b786aacf4631054ad
Firex18ru <Firex1@yandex.ru>
2024-05-26 15:26:54 +0400

    HW product

---
+++ model.py
@@ -0,0 +1,67 @@
+class Product:
+    """
+    Класс продукта
+    """
+    name: str
+    price: float
+    description: str
+    quantity: int
+
+    def __init__(self, name, price, description, quantity):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.quantity = quantity
+
+    def check_quantity(self, quantity) -> bool:
+        return self.quantity >= quantity
+
+
+    def buy(self, quantity):
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError("продукта нет в наличии")
+
+    def __hash__(self):
+        return hash(self.name + self.description)
+
+
+class Cart:
+
+
+    # Словарь продуктов и их количество в корзине
+    products: dict[Product, int]
+
+    def __init__(self):
+        # По-умолчанию корзина пустая
+        self.products = {}
+
+    def add_product(self, product: Product, buy_count=1):
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
+        raise NotImplementedError
+
+    def remove_product(self, product: Product, remove_count=None):
+        """
+        Метод удаления продукта из корзины.
+        Если remove_count не передан, то удаляется вся позиция
+        Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
+        """
+        raise NotImplementedError
+
+    def clear(self):
+        raise NotImplementedError
+
+    def get_total_price(self) -> float:
+        raise NotImplementedError
+
+    def buy(self):
+        """
+        Метод покупки.
+        Учтите, что товаров может не хватать на складе.
+        В этом случае нужно выбросить исключение ValueError
+        """
+        raise NotImplementedError
